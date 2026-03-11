2516707ce96648533e4b7f80eaabb1c518534579
Sebastian Schuhmacher <85761348+Gegrincht@users.noreply.github.com>
2026-03-11 22:56:36 +0100

    Add Logger class for logging functionality

---
+++ Chess/src/Logger.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+#define GREEN  "\033[32m"
+#define YELLOW "\033[33m"
+#define RED    "\033[31m"
+#define RESET  "\033[0m"
+
+class Logger {
+private:
+    std::string prefix = "GLOBAL";
+
+    Logger(); // Konstruktor privat, Singleton Pattern
+
+public:
+    static bool active;
+
+    // ---------------- TempLogger ----------------
+    class TempLogger {
+    private:
+        const Logger& base;
+        std::string tempPrefix;
+
+    public:
+        TempLogger(const Logger& logger, const std::string& tp = "");
+
+        void info(const std::string& msg) const;
+        void warn(const std::string& msg) const;
+        void error(const std::string& msg) const;
+    };
+
+    // ---------------- Singleton Zugriff ----------------
+    static Logger& get();
+
+    // ---------------- Logger Methoden ----------------
+    void changePrefix(const std::string& p);
+    void info(const std::string& msg) const;
+    void warn(const std::string& msg) const;
+    void error(const std::string& msg) const;
+
+    TempLogger tprefix(const std::string& tp) const;
+};
